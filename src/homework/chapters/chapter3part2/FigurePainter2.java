5f9d4e67e51e560846d3694e0fe9d51c1d5759fb
Maria Avetisyan <maravetis.96.08@gmail.com>
2022-05-15 12:17:26 +0400

    First commit! Figure painter2

--- src/homework/chapters/chapter3part2/FigurePainter2.java
+++ src/homework/chapters/chapter3part2/FigurePainter2.java
@@ -14,33 +14,38 @@ public class FigurePainter2 {
         }
         System.out.println();
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 3; j > i; j--) {
-                System.out.print("  ");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 3; j >= i; j--) {
+                System.out.print(" ");
 
             }
-            for (int j = 0; j <=i; j++) {
-                System.out.print("*  ");
+            for (int j = 0; j < i; j++) {
+                System.out.print(" *");
 
             }
             System.out.println();
         }
-        for (int i = 0; i <= 4; i++) {
-            for (int j = 0; j <i; j++) {
-                System.out.print ( " ");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
 
             }
-            for (int j = 4; j >i ; j--) {
-                System.out.print(  " * ");
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
 
             }
+            {
+                for (int j = i; j < 4; j++) {
+                    System.out.print(" ");
 
-            System.out.println();
-        }
+                }
+                System.out.println();
 
 
-
+            }
         }
     }
+}
+
 
 
